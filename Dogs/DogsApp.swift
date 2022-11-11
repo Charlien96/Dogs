adfb403736133e55c7f1cb6018de910d78536e5d
Charles Newman <chaz.new96@gmail.com>
2022-11-11 14:35:14 +0000

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Admin on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
